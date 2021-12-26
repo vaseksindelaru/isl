45c4daeae041ddca227d04feeac6f78a827ef357
vaclav sindelar <apple@vaclavs-MacBook-Pro.local>
2021-12-26 20:00:29 +0100

    map

--- isl/src/componentes/Formulario.js
+++ isl/src/componentes/Formulario.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 function Formulario({note}) {
- const {id} = note[3]
+  console.log (note)
+  return (
+    <div>
+      {note.map((not)=> {
+      return <p key={not.id}>{not.content}</p>
+      })}
+    </div>
+  )
+  /*  const {id} = note[3]
  return (<div>
      <h3>{id}</h3>
      <h1>{note[3].content}</h1> 
      <p>{note[3].description}</p>
-        </div>
-  )
+        </div> 
+  )  */
 }
 
 export default Formulario
